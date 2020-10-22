11a9cd459b72e76ec5b7cea86931afaeea25766d
Dylan Attwater <attwater@4231@saskpolytech.ca>
2020-10-22 14:38:22 -0600

    Worked on code stubs for Point System Controller -Dylan

---
+++ src/Controller/PointSystemController.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PointSystemController extends AbstractController
+{
+    public static function viewPoints()
+    {
+        http_response_code(501); //Not implemented
+    }
+}
