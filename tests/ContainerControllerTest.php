f3048a4376c0877af5fc2a60f12a344255b59293
Lane Lockhart <lockhart0905@saskpolytech.ca>
2020-10-27 15:09:22 -0600

    Test Commit

---
+++ tests/ContainerControllerTest.php
@@ -0,0 +1,111 @@
+<?php
+
+
+namespace App\Tests;
+
+
+class ContainerControllerTest
+{
+    public function TestDisplayMulti()
+    {
+
+    }
+
+    public function TestDisplaySingle()
+    {
+
+    }
+
+    public function TestDisplayMax()
+    {
+
+    }
+
+    public function TestDisplayTooMany()
+    {
+
+    }
+
+    public function TestDisplayTooLittle()
+    {
+
+    }
+
+    public function TestDisplaySpecInfo()
+    {
+
+    }
+
+    public function SiteIDLowBoundary()
+    {
+
+    }
+
+    public function SiteIDLHighBoundary()
+    {
+
+    }
+
+    public function SiteIDOutLowBoundary()
+    {
+
+    }
+
+    public function SiteIDOutHighBoundary()
+    {
+
+    }
+
+    public function SerialNumLowBoundary()
+    {
+
+    }
+
+    public function SerialNumHighBoundary()
+    {
+
+    }
+
+    public function SerialNumOutLowBoundary()
+    {
+
+    }
+
+    public function SerialNumOutHighBoundary()
+    {
+
+    }
+
+    public function LocationNumLowBoundary()
+    {
+
+    }
+
+    public function LocationNumHighBoundary()
+    {
+
+    }
+
+    public function LocationNumOutLowBoundary()
+    {
+
+    }
+
+    public function LocationNumOutHighBoundary()
+    {
+
+    }
+
+    public function ContaminationTest()
+    {
+
+    }
+
+    public function GraffitiTest()
+    {
+
+    }
+
+
+
+}
