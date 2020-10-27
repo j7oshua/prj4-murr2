b28ad1f80f00b313945193f51b2062a367f6f95d
RaeAnne Hamel <hamel8982@saskpolytech.ca>
2020-10-27 14:22:48 -0600

    Made a Test file for story19, add the testing code stubs for it, need to add code comments to it, next will make the cod stub for the controller --RH

---
+++ tests/ViewEducationalInformationTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ViewEducationalInformationTest extends TestCase
+{
+    public function pageIsAccessibleWithNoContent()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function pageIsNotAvaiable()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function pageIsAccessibleWithAnImageDisplayed()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function pageIsAccessibleAndTextIsDisplayedInDifferentStylesAndFonts()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function pageIsAccessibleAndALinkIsDisplayed()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function pageIsAccessibleAndCanViewItFromAMobileView()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function pageIsAccessibleAndCanViewItFromATabletView()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function pageIsAccessibleAndCanViewItFromADisplayView()
+    {
+        $this->assertTrue(true);
+    }
+
+
+}
