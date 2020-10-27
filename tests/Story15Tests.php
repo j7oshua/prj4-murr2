993bb01e7ed0dadb901297714a52651768676aee
Lucas Tom <tom1608@saskpolytech.ca>
2020-10-27 14:53:06 -0600

    Added more testing code -LT
    Signed-off-by: Lucas Tom <tom1608@saskpolytech.ca>

--- tests/Story15Tests.php
+++ tests/Story15Tests.php
@@ -5,6 +5,50 @@ namespace App\Tests;
 
 class Story15Tests{
 
+    public function TextPickedUP(){
+
+    }
+
+    public function TestNotPickedUp(){
+
+    }
+
+    public function TestInaccessible(){
+
+    }
+
+    public function TestContaminated(){
+
+    }
+
+    public function TestConstruction(){
+
+    }
+
+
+    public function TestOthersuccess(){
+
+    }
+
+    //give success on minimum characters 4
+    public function TestOtherMin(){
+
+    }
+
+    //give success on maximum characters 30
+    public function TestOtherMax(){
+
+    }
+
+    //give error when not between 4 and 30 characters or empty
+    public function TestOtherError(){
+
+    }
+
+    //make sure he selected one option
+    public function TestSubmitValidation(){
+
+    }
 
 
 }
