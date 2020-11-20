26cac3b6c2ae99a759cf360424b0a5bfec2a593c
Ernesto Basoalto <basoalto@saskpolytech.ca>
2020-11-19 22:59:17 -0600

    made some basic code and added the vue addon I want to use. incomplete currently

--- murr-ui/src/main.js
+++ murr-ui/src/main.js
@@ -1,7 +1,24 @@
+// add libraries
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// add css
+import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootswatch/dist/sketchy/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueEllipseProgress from 'vue-ellipse-progress';
+
+
+// add libraries to vue context
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(VueAxios, axios)
+Vue.use(VueEllipseProgress);
 
 Vue.config.productionTip = false
 
