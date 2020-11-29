b3381bc5de8bdb53111151d1a44f43c764d6858f
Joshua Grimard <grimard5500@saskpolytech.ca>
2020-11-29 17:33:39 -0600

    Finished creating code stubs for the Resident Entity and Repository --Josh

--- murr-api/src/Repository/ResidentRepository.php
+++ murr-api/src/Repository/ResidentRepository.php
@@ -14,37 +14,5 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class ResidentRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, Resident::class);
-    }
 
-    // /**
-    //  * @return Resident[] Returns an array of Resident objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('r')
-            ->andWhere('r.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('r.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Resident
-    {
-        return $this->createQueryBuilder('r')
-            ->andWhere('r.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
