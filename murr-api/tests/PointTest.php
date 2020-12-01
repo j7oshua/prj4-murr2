2ac8f830ace3b01de4ec33410f0b562a52197a77
RaeAnne Hamel <hamel8982@saskpolytech.ca>
2020-12-01 15:10:46 -0600

    Add code comments for my test, deleted the delete me tests, updated some of the tests that I think I was missing items ==RH

--- murr-api/tests/PointTest.php
+++ murr-api/tests/PointTest.php
@@ -9,17 +9,22 @@ use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
 use App\Entity\Point;
 class PointTest extends ApiTestCase
 {
+   //refreshes the database with each test
     use RefreshDatabaseTrait;
 
+    //Declare Variables
     private static $client;
     private static $repo;
     private $dataArray;
+
+   //Display Constraints
     const VIOLATION_ARRAY=[
         '@context' => '/contexts/ConstraintViolationList',
         '@type' => 'ConstraintViolationList',
         'hydra:title' => 'An error occurred'
     ];
 
+    //calls API URL
     const API_URL = '127.0.0.1:800/api/points';
 
     /**
@@ -27,7 +32,10 @@ class PointTest extends ApiTestCase
      */
     public static function setUpBeforeClass()
     {
+        // creates client
         self::$client = static::createClient();
+
+        //creates repository
         self::$repo = static::$container->get('doctrine')->getRepository(Point::class);
     }
 
@@ -43,40 +51,43 @@ class PointTest extends ApiTestCase
         ];
     }
 
-
     /**
-     * this test will test will a user with zero points
+     * this test will test will display resident_id =1 with point=0
      */
     public function testUserWithZeroPoints(): void
     {
+        //call client to do a get request and get the json from dataArray
         $response = self::$client->request('GET', self::API_URL, ['json' => $this->dataArray]);
 
+        //Validate the Get request
         $this->assertResponseStatusCodeSame(200);
         $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
         $this->assertJsonContains([
             '@context' => '/contexts/Point',
             '@type' => 'Point',
-            ...$this->dataArray,
-            'reviews' => [],
+            ...$this->dataArray
         ]);
         $this->assertRegExp('~^/points/\d+$~', $response->toArray()['@id']);
         $this->assertMatchesResourceItemJsonSchema(Point::class);
     }
 
     /**
-     * this test will test will a user with three points
+     * this test will test will display resident_id =2 with point=3
      */
     public function testUserWithThreePoints()
     {
-
+        //clear the values for the variables of the dataArray
         unset($this->dataArray['resident_id']);
         unset($this->dataArray['point']);
 
-        $response = self::$client->request('GET', self::API_URL, ['json' => $this->dataArray]);
-
+        //set new values
         $this->dataArray['resident_id'] = '2';
         $this->dataArray['point'] = '3';
 
+        //call client to do a get request and get the json from dataArray
+        $response = self::$client->request('GET', self::API_URL, ['json' => $this->dataArray]);
+
+        //Validate the Get request
         $this->assertResponseStatusCodeSame(200);
         $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
 
@@ -92,18 +103,22 @@ class PointTest extends ApiTestCase
     }
 
     /**
-     * this test will test will a user with eighty points
+     * this test will test will display resident_id =2 with point=80
      */
     public function testUserWithEightyPoints()
     {
+        //clear the values for the variables of the dataArray
         unset($this->dataArray['resident_id']);
         unset($this->dataArray['point']);
 
-        $response = self::$client->request('GET', self::API_URL, ['json' => $this->dataArray]);
-
+        //set new values
         $this->dataArray['resident_id'] = '3';
         $this->dataArray['point'] = '80';
 
+        //call client to do a get request and get the json from dataArray
+        $response = self::$client->request('GET', self::API_URL, ['json' => $this->dataArray]);
+
+        //Validate the Get request
         $this->assertResponseStatusCodeSame(200);
         $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
 
