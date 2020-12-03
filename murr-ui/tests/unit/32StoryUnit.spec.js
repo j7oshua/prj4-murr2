c00e95a7d2299c4d1239925a481b336cfffe8f03
Lucas Tom <tom1608@saskpolytech.ca>
2020-12-03 12:41:17 -0600

    I think I have a good outline of automated testing code and I am hoping to get the go-ahead on the pre-code very soon.
    Signed-off-by: Lucas Tom <tom1608@saskpolytech.ca>

--- murr-ui/tests/unit/32StoryUnit.spec.js
+++ murr-ui/tests/unit/32StoryUnit.spec.js
@@ -3,7 +3,7 @@ import { shallowMount } from '@vue/test-utils'
 import Progress from '@/components/ProgressPoints.vue'
 
 describe('ProgressPoints.vue', () => {
-  it('renders props.msg when passed', () => {
+  it('renders props.msg when passed success', () => {
     const msg = 'Great Job!'
     const wrapper = shallowMount(Progress, {
       propsData: { msg }
@@ -11,11 +11,35 @@ describe('ProgressPoints.vue', () => {
     expect(wrapper.text()).to.include(msg)
   })
 
-  it('renders props.value when passed', () => {
+  it('does not render props.msg when passed shows ERROR', () => {
+    const msg = 'Sorry, Runtime Error: 443. Go back to previous page'
+    const wrapper = shallowMount(Progress, {
+      propsData: { msg }
+    })
+    expect(wrapper.text()).to.include(msg)
+  })
+
+  it('renders props.value when passed 1', () => {
     const PointValue = 1
     const wrapper = shallowMount(Progress, {
       propsData: { PointValue }
     })
     expect(wrapper.text()).to.include(PointValue)
+  })
+
+  it('renders props.value when passed 0', () => {
+    const PointValue = 0
+    const wrapper = shallowMount(Progress, {
+      propsData: { PointValue }
+    })
+    expect(wrapper.text()).to.include(PointValue)
+  })
+
+  it('ERROR when passed null or no connected account', () => {
+    const PointValue = null
+    const wrapper = shallowMount(Progress, {
+      propsData: { PointValue }
+    })
+    expect(wrapper.text()).to.include('Sorry, Error: 442. Go back and log in or wait for your account to be created')
   })
 })
