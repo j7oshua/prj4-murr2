d09c1567a9a50c7a51cb7b794b913ba148d67b91
Lane Lockhart <lockhart0905@saskpolytech.ca>
2020-12-08 12:33:37 -0600

    added some unit test stubs -Lane L

--- murr-ui/tests/unit/UnitTesting.js
+++ murr-ui/tests/unit/UnitTesting.js
@@ -15,26 +15,57 @@ import {shallowMount} from "@vue/test-utils";
 describe('ProfileForm', () => {
   it('renders a profile form Screen and responds correctly to user input', () => {
     const wrapper = shallowMount(ProfileForm, {
-      data() {
-        return {
           firstName: 'Lane',
           lastName: 'Lockhart',
           address: '222 seventh st west',
           city: 'Warman',
           province: 'SK',
           postalCode: 'S0K4S0'
-        }
-      }
     });
 
     // see if the message renders
-    expect(wrapper.find('.firstName').text()).toEqual('Lane');
+    expect(wrapper.find('.firstName').text()).to.include('Lane');
+    expect(wrapper.find('.lastName').text()).to.include('Lockhart');
+    expect(wrapper.find('.address').text()).to.include('222 seventh st west');
+    expect(wrapper.find('.city').text()).to.include('Warman');
+    expect(wrapper.find('.province').text()).to.include('SK');
+    expect(wrapper.find('.postalCode').text()).to.include('S0K4S0');
 
+    expect(wrapper.find('.error').isEmpty());
+
+
+
+
+    // update the `firstName` and error should exist
+    wrapper.setData({ firstName: 'ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss' });
     // assert the error is rendered
-    expect(wrapper.find('.error').exists()).toBeTruthy();
+    expect(wrapper.find('.error').exists());
 
-    // update the `username` and assert error is no longer rendered
-    wrapper.setData({ firstName: '' });
-    expect(wrapper.find('.error').exists()).toBeFalsy();
+    wrapper.setData({ firstName: 'Lane' });
+    wrapper.setData({lastName: 'sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss'});
+    // assert the error is rendered
+    expect(wrapper.find('.error').exists());
+
+    wrapper.setData({ lastName: 'Lockhart' });
+    wrapper.setData({address: 'ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss'});
+    // assert the error is rendered
+    expect(wrapper.find('.error').exists());
+
+    wrapper.setData({ address: '222 seventh st west' });
+    wrapper.setData({ city: 'ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss'});
+    // assert the error is rendered
+    expect(wrapper.find('.error').exists());
+
+    wrapper.setData({ city: 'Warman' });
+    wrapper.setData({province: 's'});
+    // assert the error is rendered
+    expect(wrapper.find('.error').exists());
+
+    wrapper.setData({ province: 'SK' });
+    wrapper.setData({postalCode: 's'});
+    // assert the error is rendered
+    expect(wrapper.find('.error').exists());
   })
+
 });
+
