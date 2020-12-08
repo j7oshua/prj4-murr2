4f1b5a2c3dd1a919bf14324ff994d3826c97f2b2
Tyson Leibel <leibel4702@saskpolytech.ca>
2020-12-08 14:50:59 -0600

    renamed test file to be more clear, changed the folder structure to remove home and about vues

--- murr-ui/src/router/index.js
+++ murr-ui/src/router/index.js
@@ -6,16 +6,8 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue')
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "home" */ '@/views/Login.vue')
   }
 ]
 
