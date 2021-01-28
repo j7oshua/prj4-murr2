a257808cb06eec679635f394be4e5bc40634b897
Julius Tuba <julius3591@saskpolytech.ca>
2021-01-28 13:43:46 -0600

    Demo'd the code to Wade and cleaned up some unneeded comments. Ready to merge to master -- Julius and Tyson

--- murr-api/src/Validator/PhoneAndEmailBothLeftBlankValidator.php
+++ murr-api/src/Validator/PhoneAndEmailBothLeftBlankValidator.php
@@ -11,15 +11,10 @@ class PhoneAndEmailBothLeftBlankValidator extends ConstraintValidator
 {
     public function validate($entity, Constraint $constraint) : void
     {
-        /*THIS STILL NEEDS TO BE WORKED ON. THIS IS JUST DEFAULT CODE*/
-       // /* @var $constraint \App\Validator\PhoneAndEmailBothLeftBlankValidator */
-
         if (null === $entity || '' === $entity) {
             return;
         }
 
-
-        // TODO: implement the validation here
         if ($entity->getEmail() == "" && $entity->getPhone() == "" || is_null($entity->getPhone()) && is_null($entity->getEmail())) {
             $this->context->buildViolation($constraint->message)
                 ->atPath('email')
