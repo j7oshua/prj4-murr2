f39193b2355748662432c56369995643badc27f0
Joshua Grimard <grimard5500@saskpolytech.ca>
2021-02-01 13:50:53 -0600

    Worked on creating the routing for the story --Josh

--- murr-ui/src/App.vue
+++ murr-ui/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <b-nav pills align="center" class="mb-4">
+  <!--  <b-nav pills align="center" class="mb-4">
       <b-nav-item to="/" exact-active-class="active">Home</b-nav-item>
       <b-nav-item to="/login" exact-active-class="active">Login</b-nav-item>
-    </b-nav>
+    </b-nav>-->
     <router-view class="container-xl"/>
   </div>
 </template>
