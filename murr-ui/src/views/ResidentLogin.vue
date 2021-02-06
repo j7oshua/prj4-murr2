101fd84edac467d857bbdcd5ba32d524636cb712
Lucas Tom <tom1608@saskpolytech.ca>
2021-02-06 16:06:41 -0600

    working on code stubs.

---
+++ murr-ui/src/views/ResidentLogin.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!--<ResidentLoginForm ref="ResidentLogin" @added="handleLogin" /> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ResidentLogin',
+  components: {
+    // ResidentLoginForm: ResidentLogin
+  },
+  methods: {
+    handleLogin () {
+    },
+    callAPI () {
+      // throw new Error('Not Implemented')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
