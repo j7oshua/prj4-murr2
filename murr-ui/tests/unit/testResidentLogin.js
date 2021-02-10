3a112887bffb4d8edbee8d9b4c8c662988382b8f
Dylan Attwater <attwater@4231@saskpolytech.ca>
2021-02-10 14:32:21 -0600

    Updated tests based off wades feedback

--- murr-ui/tests/unit/testResidentLogin.js
+++ murr-ui/tests/unit/testResidentLogin.js
@@ -9,47 +9,25 @@ describe('ResidentLogin.vue', () => {
   it('Resident with correct login credentials', async () => {
     const loginInfo = 'test@email.com'
     const password = 'password'
+    const route = 'point/resident'
     const request = nock('http://localhost:3000/login')
       .get(`/login?login-info=${loginInfo}&password=${password}`)
       .reply(200)
     const wrapper = mount(ResidentLogin)
-    expect(request).contains
-
-    expect(wrapper.attributes('loginInfo').to.be(loginInfo))
-    // expect(wrapper.text()).to.include('Points :') // if you at home page
-    // you will need to check the home page path
-
-      Vue.use(VueRouter)
-      const router = new VueRouter({
-        routes: [
-          { path: '/totest/:id', name: 'totest', component: ResidentLogin },
-          { path: '/wherever', name: 'another_component', component: { render: h => '-' } }
-        ]
-      })
-      const vm = new Vue({
-        el: document.createElement('div'),
-        router: router,
-        render: h => h('router-view')
-      })
-      router.get({ name: 'totest', params: { id: 123 } })
-      Vue.nextTick(() => {
-        console.log('html:', vm.$el)
-        expect(vm.$el.querySelector('h2').textContent).to.equal('Fred Bloggs')
-        done()
-      })
-
+    expect(request.statusCode()).equals(404)
+    expect(wrapper).route.equals(route) //Redirected to resident home page
   })
 
   // Incorrect login information will have a error message
   it('Resident with wrong login credentials', async () => {
     const loginInfo = 'testemail.com'
-    const password = 'password'
+    const password = 'pssword'
     const request = nock('http://localhost:3000/login')
       .get(`/login?login-info=${loginInfo}&password=${password}`)
       .reply(404)
     const wrapper = mount(ResidentLogin)
-    expect(wrapper.attributes('loginInfo').to.be(loginInfo))
     const msg = 'Login failed. Please try again.'
-    expect(wrapper.text()).to.include(msg) // if you get the error message
+    expect(request.statusCode()).equals(404)
+    expect(wrapper.text()).to.include(msg) //Error message appears
   })
 })
