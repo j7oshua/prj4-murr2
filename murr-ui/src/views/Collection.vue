016edfc428c56e3f96fbf70883c422c81917256e
Julius Tuba <julius3591@saskpolytech.ca>
2021-02-25 14:04:38 -0600

    Committing again since the last commit did not include the view that I created. - Julius

---
+++ murr-ui/src/views/Collection.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'Collection',
+  data () {
+    return {
+      pickUp: {
+        pickUpID: 1,
+        siteID: 1,
+        numCollected: 5,
+        numObstructed: 0,
+        numContaminated: 0,
+        date: "24-02-2021",
+      }
+    }
+  }
+}
+
+
+</script>
+
+
+<style scoped>
+
+</style>
