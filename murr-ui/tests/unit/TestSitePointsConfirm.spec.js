6d9480085a541a26fd3dfc2456dddfd56e082442
Julius Tuba <julius3591@saskpolytech.ca>
2021-03-01 14:52:55 -0600

    Continued working on the front end tests. Added tests for when the front end sends the post request to the back end. - Joshua and Julius

--- murr-ui/tests/unit/TestSitePointsConfirm.spec.js
+++ murr-ui/tests/unit/TestSitePointsConfirm.spec.js
@@ -3,7 +3,12 @@ import SitePointsConfirmation from '@/components/SitePointsConfirmation'
 import { expect } from 'chai'
 
 let wrapper
+const server = '127.0.0.1:8000'
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+chai.use(chaiHttp)
 
+// ******* Setup the props named pickup to contain the values declared below before each test
 describe('SitePointsConfirmation.vue', () => {
   beforeEach(() => {
     wrapper = mount(SitePointsConfirmation, {
@@ -37,6 +42,7 @@ describe('SitePointsConfirmation.vue', () => {
         }
       })
     })
+    // ******** Test block when there is no pickup ID found in the database
     it('displays error that pickupID was not found', () => {
       expect(wrapper.find('h1').text().to.be.equal('Error'))
       expect(wrapper.find('.errorMessage').text().to.be.equal('Pickup ID was not found'))
@@ -46,25 +52,28 @@ describe('SitePointsConfirmation.vue', () => {
         resp: 500
       })
     })
-    it('displays error that could not connect to server',() => {
+    // ******* Test for when the front end could not communicate with the backend
+    it('displays error that could not connect to server', () => {
       expect(wrapper.find('h1').text().to.be.equal('Connection Error'))
       expect(wrapper.find('.errorMessage').text().to.be.equal('Could not connect to the server'))
     })
   })
-  // *********
+  // ********* Testing the cancel button for the modal. Should close the modal when the button is clicked
   describe('clicking the cancel button', async () => {
     it('closes the component', () => {
       wrapper.find('button.cancel').trigger('click')
       expect(wrapper.exists().to.be.false)
     })
   })
-  // **********
+  // ********** Test block for the yes button.
   describe('clicking the yes button', () => {
+    // ************ Test block for when points are added to the site with a success message
     it('displays success message that points were added', async () => {
       wrapper.find('button.yes').trigger('click')
       expect(wrapper.find('h1').text().to.be.equal('Points Added!'))
       expect(wrapper.find('.successMessage').text().to.be.equal('Successfully added points to { sitename }!'))
     })
+    // ******* Test block for having no points added to the site
     it('displays message that no points were added', async () => {
       // Set the pickup prop to now include a pickup with no containers collected
       await wrapper.setProps({
@@ -80,6 +89,32 @@ describe('SitePointsConfirmation.vue', () => {
       wrapper.find('button.yes').trigger('click')
       expect(wrapper.find('h1').text().to.be.equal('{ sitename } No Points Added'))
       expect(wrapper.find('.successMessage').text().to.be.equal('No points were added to { sitename }'))
+    })
+  })
+  describe('post to the database', () => {
+    it('creates points for the site and returns status code 201', (done) => {
+      const newPickup = {
+        pickupID: 1
+      }
+      chai.request(server)
+        .post('/api/site/1')
+        .send(newPickup)
+        .end((res) => {
+          expect(res.should.have.status(201))
+          expect(res.body.should.be.a('string'))
+          done()
+        })
+    })
+    it('error while sending no pickup ID', (done) => {
+      const newPickup = {}
+      chai.request(server)
+        .post('/api/site/1')
+        .send(newPickup)
+        .end((res) => {
+          expect(res.should.have.status(400))
+          expect(res.body.should.be.a('string'))
+          done()
+        })
     })
   })
 })
