d03f6e1d27231b8fdccba0ea85cc1f09ef9373cb
RaeAnne Hamel <hamel8982@saskpolytech.ca>
2021-03-02 00:22:52 -0600

    Started at 11:30, Finished Frontend code stubs (16 stubs) and code comments for TestDriverSetsCollection.spec.js --Rae

--- murr-ui/tests/unit/TestDriverSetsCollection.spec.js
+++ murr-ui/tests/unit/TestDriverSetsCollection.spec.js
@@ -9,40 +9,163 @@ describe('DriverSiteReport.vue', () => {
   // create a new wrapper container for the shallow mount
 
   /**
-   * Title: Driver submits collected, contaminated, obstructed containers on site 1
-   * Purpose: This test will test that ALL fields are correctly inputted and returned valid
+   * Title: TestContaminatedFour
+   * Purpose: Test a pickup history of only contaminated bins
    * Expected Result: Success
-   * Return: return to collection page
+   * Return: return Collections History Form
    **/
-  it('Should successfully be valid with all fields entered', async () => {
+  it('Should successfully be valid with all bins contaminated', async () => {
 
   })
 
   /**
-   * Title: Driver submits three collected containers on site 1
-   * Purpose: This test will test if the driver incorrectly inputs a lower number than required
+   * Title: TestObstructedFour
+   * Purpose: Test a pickup history of only obstructed bins
+   * Expected Result: Success
+   * Return: return Collections History Form
+   **/
+  it('Should successfully be valid with all bins obstructed', async () => {
+
+  })
+
+  /**
+   * Title: TestCollectedFour
+   * Purpose: Test a pickup history of only collected bins
+   * Expected Result: Success
+   * Return: return Collections History Form
+   **/
+  it('Should successfully be valid with all bins collected', async () => {
+
+  })
+  /**
+  * Title: TestCollectedObstructedContaminated
+   * Purpose: Test a pickup history of all bin types
+   * Expected Result: Success
+   * Return: return Collections History Form
+  **/
+  it('Should successfully be valid with all bins types', async () => {
+
+  })
+
+  /**
+   * Title: TestCollectedObstructedFour
+   * Purpose: Test a pickup history of only collected and contaminated bins
+   * Expected Result: Success
+   * Return: return Collections History Form
+   **/
+  it('Should successfully be valid with all bins collected and obstructed', async () => {
+
+  })
+
+  /**
+   * Title: TestCollectedContaminatedFour
+   * Purpose: Test a pickup history of only collected and contaminated bins
+   * Expected Result: Success
+   * Return: return Collections History Form
+   **/
+  it('Should successfully be valid with all bins collected and contaminated', async () => {
+
+  })
+
+  /**
+   * Title: TestContaminatedObstructed
+   * Purpose: Test a pickup history of only contaminated and obstructed bins
+   * Expected Result: Success
+   * Return: return Collections History Form
+   **/
+  it('Should successfully be valid with all bins contaminated and obstructed', async () => {
+
+  })
+
+  /**
+   * Title: TestMissingBinError
+   * Purpose: This test will test if the driver inputs a total of bins less than the
+   *          number of bins associated to a site
    * Expected Result: Failure
    * Return: Error Message description: "Error - Invalid number of containers".
    **/
-  it('Should display error when invalid container number is entered', async () => {
+  it('Should display error when container number of containers is less than 4', async () => {
 
   })
 
   /**
-   * Title: Driver submits no containers on site 1
-   * Purpose: This test will test if the driver incorrectly inputs nothing
+   * Title: TestMaxBinError
+   * Purpose: This test will test if the driver inputs a total of bins
+   *          more than the number of bins associated to a site
    * Expected Result: Failure
-   * Return: Error Message description: "Error - Invalid require Bin number amount".
+   * Return: Error Message description: "Error - Invalid number of containers".
+   **/
+  it('Should display error when container number of containers is more than 4', async () => {
+
+  })
+
+  /**
+   * Title: TestNullBinError
+   * Purpose: This test will test if the driver inputs null bins
+   * Expected Result: Failure
+   * Return: Error Message description: "Error - Invalid, Bin number amount required".
+   **/
+  it('Should display error when container number of containers is null', async () => {
+
+  })
+
+  /**
+   * Title: TestPastDateTimeError
+   * Purpose: This test will test Past date input
+   * Expected Result: Failure
+   * Return: Error Message description: "Error - Invalid date response".
+   **/
+  it('Should display error when date fields are in a past date', async () => {
+
+  })
+
+  /**
+   * Title: TestFutureDateTimeError
+   * Purpose: This test will test future date input
+   * Expected Result: Failure
+   * Return: Error Message description: "Error - Invalid date response".
+   **/
+  it('Should display error when date fields are in a future date', async () => {
+
+  })
+
+  /**
+   * Title: TestNullDate
+   * Purpose: This test will test null date input
+   * Expected Result: Failure
+   * Return: Error Message description: "Error – Invalid, date required".
+   **/
+  it('Should display error when date fields are null', async () => {
+
+  })
+
+  /**
+   * Title: TestAllFieldsBlank
+   * Purpose: This test will test if all fields are left null
+   * Expected Result: Failure
+   * Return: Error Message description: "Error – Invalid, date required".
+   *         Error Message description: "Error - Invalid require Bin number amounts".
+   **/
+  it('Should display error when all fields are null', async () => {
+
+  })
+
+  /**
+   * Title: TestNoSiteShown
+   * Purpose: This test will test if there is no site
+   * Expected Result: Failure
+   * Return: Error Message description: "Error -No Site Exist".
    **/
   it('Should display error when container fields are required', async () => {
 
   })
+
   /**
-  * Title: Driver submits no containers on site 1
-  * Purpose: This test will test if the driver incorrectly inputs nothing
-  * Expected Result: Failure
-  * Return: Error Message description: "Error - Invalid require Bin number amount".
-  **/
+   * Title: TestLoadPageError
+   * Purpose: This test will test failure for page to load
+   * Expected Result: Failure
+   * Return: Error Message description: "Error - Invalid Page/ Not Found".
+   **/
   it('Should display error when container fields are required', async () => {
 
   })
