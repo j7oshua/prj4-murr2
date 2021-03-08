6cc44391732a11ffeb80ed25b84c2f588db6fefc
Lucas Tom <tom1608@saskpolytech.ca>
2021-03-08 13:30:09 -0600

    Getting ready for pre-code.

---
+++ murr-ui/src/mixins/global-mixin.js
@@ -0,0 +1,10 @@
+// This will be used by everyone so we will move everything to this global mixin file
+const PICKUP_URL = ''
+const PickupMixin = ({
+  methods: {
+    callAPI: function (method, data) {
+
+    }
+  }
+})
+export default PickupMixin
