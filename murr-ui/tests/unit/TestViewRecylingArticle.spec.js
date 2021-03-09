8ada045fbb8cbc4d8a88afda381a5809830f24cf
Dylan Attwater <attwater@4231@saskpolytech.ca>
2021-03-09 13:54:18 -0600

    Got all front end tests passing using chai and mocha

--- murr-ui/tests/unit/TestViewRecylingArticle.spec.js
+++ murr-ui/tests/unit/TestViewRecylingArticle.spec.js
@@ -1,46 +1,53 @@
-import nock from 'nock'
-import flushPromises from 'flush-promises'
-import { expect } from "chai"
-import {describe} from "mocha";
+const request = require('supertest')('http://127.0.0.1:8000/api')
+const expect = require('chai').expect
 
-describe('AllArticles.vue', () => {
-  // this test will make sure the get request is returning the full list of articles available
-  it('Resident views all recycling articles on main page', async () => {
-    const request = nock('http://localhost:8000/api/articles')
-      // make the call to the mock database passing in the article id
-      .get(`/api/articles`)
-      .reply(200)
-    await flushPromises()
-    // should return a status code of 200
-    expect(request).to.contain({id: 1, title: "What Can You Recycle", image: "http://image1.jpg"})
-    expect(request).to.contain({id: 2, title: "How to Recycle", image: "http://image2.jpg"})
-    expect(request).to.contain({id: 3, title: "Hours and Location", image: "http://image3.jpg"})
-  })
-})
+// npm install --save supertest mocha chai
 
-describe('ArticleDetails.vue', () => {
-  // this test make sure the get request is working for that specific article and displaying the correct information
-  it('Resident selects recycling article “What can you Recycle”', async () => {
-    const expectedArticle = 1
-    const request = nock('http://localhost:8000/api/articles/1')
-      // make the call to the mock database passing in the article id
-      .get(`/api/articles/${expectedArticle}`)
-      .reply(200)
-    await flushPromises()
-    // should return a status code of 200
-    expect(request).to.contains( [{ statusCode: 200 }])
-    expect(request).to.contain({id: 1, title: "What Can You Recycle", image: "http://image1.jpg", info: "Paper, Plastic, and Cardboard"})
-    expect(request).to.contain(this.href = 'http://localhost:8000/api/articles/1')
+describe('GET /articles', function () {
+
+  /**
+   * Title: Resident views all recycling articles on main page
+   * Purpose: This test will test that ALL articles appear on the main education page
+   * Expected Result: Success
+   * Return: Status Code: 200, Array of article objects
+   **/
+  it('Resident views all recycling articles on main page', async function () {
+    const response = await request.get("/articles")
+    expect(response.status).to.eql(200)
+
+    expect(response.body['hydra:member'][0]).to.contain({ title: 'What Can You Recycle' })
+    expect(response.body['hydra:member'][1]).to.contain({ title: 'How to Recycle' })
+    expect(response.body['hydra:member'][2]).to.contain({ title: 'Hours and Location' })
+
+    expect(response.body['hydra:member'][0]).to.contain({ image: 'https://cosmoindustries.com/assets/images/combined-50th-sigfile-798x392.png' })
+    expect(response.body['hydra:member'][1]).to.contain({ image: 'http://image2url.jpg' })
+    expect(response.body['hydra:member'][2]).to.contain({ image: 'http://image3url.jpg' })
   })
-  it('Resident unsuccessfully views recycling article', async () => {
-    // this test is to make sure if the user enters an invalid article id in the url it wont work
-    const expectedArticle = -1
-    const request = nock('http://localhost:8000/api/articles/-1')
-      // make the call to the mock database passing in the article id
-      .get(`/api/articles/${expectedArticle}`)
-      .reply(404)
-    await flushPromises()
-    // should return a status code of 404
-    expect(request).to.contains([{ statusCode: 404 }])
+
+  /**
+   * Title: Resident selects recycling article “What can you Recycle"
+   * Purpose: This test will test that once a Resident selects an article they will be transferred to an article
+   *          page with that article's specific details
+   * Expected Result: Success
+   * Return: Status Code: 200, Array of an article
+   **/
+  it('Resident selects recycling article “What can you Recycle”', async function () {
+    const response = await request.get("/articles/1")
+    expect(response.status).to.eql(200)
+
+    expect(response.body).to.contain({ title: 'What Can You Recycle' })
+    expect(response.body).to.contain({ image: 'https://cosmoindustries.com/assets/images/combined-50th-sigfile-798x392.png' })
+    expect(response.body).to.contain({ info: 'Paper, Plastic, and Cardboard' })
+  })
+
+  /**
+   * Title: Resident unsuccessfully views recycling article
+   * Purpose: This test will test that if a Resident attempts to go an article that doesnt exist, they will see an error page
+   * Expected Result: Success
+   * Return: Status Code: 404
+   **/
+  it('Resident unsuccessfully views recycling article', async function () {
+    const response = await request.get("/articles/-1")
+    expect(response.status).to.eql(404)
   })
 })
