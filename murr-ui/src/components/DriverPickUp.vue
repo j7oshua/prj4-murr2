c2e6ece3570e53c58f277a5d5e2304eeab7f3914
RaeAnne Hamel <hamel8982@saskpolytech.ca>
2021-03-10 01:44:02 -0600

    Tonight I worked on my extra time (one hour), I worked on finishing the Form( added the date div, the obstructed form group, the contamined form form gorup), Added a new method called getServerDate, added the created section to implement the new method, added a custom method to help validate the bin count called checkValidBins(value1, value2, value3) though I dont know if this is needed. all these changes were done in DriverPickUp.vue --Rae

--- murr-ui/src/components/DriverPickUp.vue
+++ murr-ui/src/components/DriverPickUp.vue
@@ -14,18 +14,47 @@
       <div class="valid-feedback" id="properSiteName"></div>
       <div class="invalid-feedback" id="improperSiteName">Error - No site exists.</div>
     </div>
+    <div class="form-group col-4">
+      <label for="siteDate">Date: </label>
+      <p id="siteDate">{{dateStamp}}</p>
+    </div>
   </div>
     <div class="form-row">
       <div class="form-group col-6">
-      <label for="collected">Collected</label>
+      <label for="collected">Collected: </label>
         <input id="collected" type="text" class="form-control" v-model.trim="$v.pickup.numCollected.$model"
-               :class="{'is-invlaid' :$v.pickup.numCollected.$error, 'is-invalid': $v.pickup.numCollected}">
+               :class="{'is-invlaid' :$v.pickup.numCollected.$error, 'is-invalid': $v.pickup.numCollecte}">
         <div class="valid-feedback" id="properCollected">Valid bin amount </div>
         <div class="invalid-feedback" id="improperCollected">
           <span v-if="$v.pickup.numCollected.required">Error - Invalid. Bin number amount required.</span>
-          <span v-if="$"
+          <span v-if="$"></span>
+          <span></span>
         </div>
       </div>
+      </div>
+  <div class="form-row">
+    <div class="form-group col-6">
+      <label for="obstructed">Obstructed: </label>
+      <input id="obstructed" type="text" class="form-control" v-model.trim="$v.pickup.numObstructed.$model"
+             :class="{'is-invlaid' :$v.pickup.numObstructed.$error, 'is-invalid': $v.pickup.numObstructed}">
+      <div class="valid-feedback" id="properObstucted">Valid bin amount </div>
+      <div class="invalid-feedback" id="improperObstructed">
+        <span v-if="$v.pickup.numObstructed.required">Error - Invalid. Bin number amount required.</span>
+        <span v-if="$"></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+  <div class="form-row">
+    <div class="form-group col-6">
+      <label for="contaminated">Contaminated: </label>
+      <input id="contaminated" type="text" class="form-control" v-model.trim="$v.pickup.numContaminated.$model"
+             :class="{'is-invlaid' :$v.pickup.numContaminated.$error, 'is-invalid': $v.pickup.numContaminated}">
+      <div class="valid-feedback" id="properContaminated">Valid bin amount </div>
+      <div class="invalid-feedback" id="improperContaminated">
+        <span v-if="$v.pickup.numContaminated.required">Error - Invalid. Bin number amount required.</span>
+        <span v-if="$"></span>
+        <span></span>
       </div>
     </div>
   </div>
@@ -35,10 +64,11 @@
 
 <script>
 import { validationMixin } from 'vuelidate'
-import { required } from 'vuelidate/lib/validators'
+import { numeric, required } from 'vuelidate/lib/validators'
 
 export default {
   name: 'DriverSiteReport',
+  mixins: [validationMixin],
   props: {
     siteObject: {
       type: Object
@@ -54,34 +84,52 @@ export default {
         dateTime: '',
         siteObject: ''
       },
-      error: {}
+      error: {},
+      dateStamp: ''
     }
   },
 
   validations: {
     pickup: {
-      siteid: { // we will need to figure this one out
-      },
       numCollected: {
         required,
-        checkIfValidNumBin (value) {
-          return value
+        checkValidBins (value1, value2, value3) {
+          return false
         },
-        checkCount (value) {
-          return value
-        }
+        numeric
       },
       numObstructed: {
-        // some validation
+        required,
+        checkValidBins (value1, value2, value3) {
+          return false
+        },
+        numeric
       },
       numContaminated: {
-        // some validation
+        required,
+        checkValidBins (value1, value2, value3) {
+          return false
+        },
+        numeric
       }
     }
+  },
+  created () {
+    setInterval(this.getSeverTime, 1000)
   },
   methods: {
     postPickup: function () {
       throw new Error('Not Implemented')
+    },
+    checkValidBins (value1, value2, value3) {
+      // this will be a if else statement to check the values
+      // and compare them to determine the right response by returning true or false
+      // this check will also check against the count of the site bins
+      return true
+    },
+    getSeverTime: function () {
+      const today = new Date()
+      this.dateStamp = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
     }
   }
 }
