4da022dd851f4cba9d46609b36e43394a6d3e721
Lucas Tom <tom1608@saskpolytech.ca>
2021-03-10 16:13:54 -0600

    Commented code. added submit button. added between validation and error messages. added numBins assigned property from props. It maybe replaced when we figure out the checkValidBins function.

--- murr-ui/src/components/DriverPickUp.vue
+++ murr-ui/src/components/DriverPickUp.vue
@@ -2,72 +2,91 @@
 <div>
 <form @submit.prevent="postPickup">
   <div class="form-row">
+    <!-- This will show the site id -->
     <div class="form-group col-4">
       <label for="siteId">Site ID: </label>
       <p id="siteId">{{siteObject.id}}</p>
       <div class="valid-feedback" id="properSiteID"></div>
       <div class="invalid-feedback" id="improperSiteID">Error - No site exists.</div>
     </div>
+    <!-- This will show the site name-->
     <div class="form-group col-4">
       <label for="siteName">Site ID: </label>
       <p id="siteName">{{siteObject.siteName}}</p>
       <div class="valid-feedback" id="properSiteName"></div>
       <div class="invalid-feedback" id="improperSiteName">Error - No site exists.</div>
     </div>
+    <!-- This will show the current date from the website-->
     <div class="form-group col-4">
       <label for="siteDate">Date: </label>
       <p id="siteDate">{{dateStamp}}</p>
     </div>
   </div>
+  <!-- This will be the Collected section-->
     <div class="form-row">
       <div class="form-group col-6">
       <label for="collected">Collected: </label>
         <input id="collected" type="text" class="form-control" v-model.trim="$v.pickup.numCollected.$model"
-               :class="{'is-invlaid' :$v.pickup.numCollected.$error, 'is-invalid': $v.pickup.numCollected}">
+               :class="{'is-invalid' :$v.pickup.numCollected.$error, 'is-invalid': $v.pickup.numCollected}">
         <div class="valid-feedback" id="properCollected">Valid bin amount </div>
         <div class="invalid-feedback" id="improperCollected">
+          <!-- required error-->
           <span v-if="$v.pickup.numCollected.required">Error - Invalid. Bin number amount required.</span>
+          <!-- numeric error-->
           <span v-if="!$v.pickup.numCollected.numeric">Must contain only digits</span>
+          <!-- between error-->
+          <span v-if="!$v.pickup.numCollected.between">Error - Invalid number of bins</span>
           <span v-if="$"></span>
           <span></span>
         </div>
       </div>
       </div>
+  <!-- This will be the Obstructed section-->
   <div class="form-row">
     <div class="form-group col-6">
       <label for="obstructed">Obstructed: </label>
       <input id="obstructed" type="text" class="form-control" v-model.trim="$v.pickup.numObstructed.$model"
-             :class="{'is-invlaid' :$v.pickup.numObstructed.$error, 'is-invalid': $v.pickup.numObstructed}">
+             :class="{'is-invalid' :$v.pickup.numObstructed.$error, 'is-invalid': $v.pickup.numObstructed}">
       <div class="valid-feedback" id="properObstucted">Valid bin amount </div>
       <div class="invalid-feedback" id="improperObstructed">
+        <!-- required error-->
         <span v-if="$v.pickup.numObstructed.required">Error - Invalid. Bin number amount required.</span>
+        <!-- numeric error-->
         <span v-if="!$v.pickup.numObstructed.numeric">Must contain only digits</span>
+        <!-- between error-->
+        <span v-if="!$v.pickup.numObstructed.between">Error - Invalid number of bins</span>
         <span v-if="$"></span>
         <span></span>
       </div>
     </div>
   </div>
+  <!-- This will be the Contaminated section-->
   <div class="form-row">
     <div class="form-group col-6">
       <label for="contaminated">Contaminated: </label>
       <input id="contaminated" type="text" class="form-control" v-model.trim="$v.pickup.numContaminated.$model"
-             :class="{'is-invlaid' :$v.pickup.numContaminated.$error, 'is-invalid': $v.pickup.numContaminated}">
+             :class="{'is-invalid' :$v.pickup.numContaminated.$error, 'is-invalid': $v.pickup.numContaminated}">
       <div class="valid-feedback" id="properContaminated">Valid bin amount </div>
       <div class="invalid-feedback" id="improperContaminated">
+        <!-- required error-->
         <span v-if="$v.pickup.numContaminated.required">Error - Invalid. Bin number amount required.</span>
+        <!-- numeric error-->
         <span v-if="!$v.pickup.numContaminated.numeric">Must contain only digits</span>
+        <!-- between error-->
+        <span v-if="!$v.pickup.numContaminated.between">Error - Invalid number of bins</span>
         <span v-if="$"></span>
         <span></span>
       </div>
     </div>
   </div>
+  <button type="submit" class="btn btn-submit">Submit</button>
 </form>
 </div>
 </template>
 
 <script>
 import { validationMixin } from 'vuelidate'
-import { numeric, required } from 'vuelidate/lib/validators'
+import { numeric, required, between } from 'vuelidate/lib/validators'
 
 export default {
   name: 'DriverSiteReport',
@@ -88,10 +107,10 @@ export default {
         dateTime: ''
       },
       error: {},
-      dateStamp: ''
+      dateStamp: '',
+      numBins: 0
     }
   },
-
   validations: {
     pickup: {
       numCollected: {
@@ -99,26 +118,30 @@ export default {
         checkValidBins (value1, value2, value3) {
           return false
         },
-        numeric
+        numeric,
+        between: between(0, this.numBins)
       },
       numObstructed: {
         required,
         checkValidBins (value1, value2, value3) {
           return false
         },
-        numeric
+        numeric,
+        between: between(0, this.numBins)
       },
       numContaminated: {
         required,
         checkValidBins (value1, value2, value3) {
           return false
         },
-        numeric
+        numeric,
+        between: between(0, this.numBins)
       }
     }
   },
   created () {
     setInterval(this.getSeverDate, 1000)
+    this.numBins = Number.parseInt(this.props.siteObject.numBins)// assigned a numBins property from props numBins, not sure if I need to cast
   },
   methods: {
     postPickup: function () {
