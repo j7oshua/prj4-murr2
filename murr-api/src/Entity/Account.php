94ca7212c8c28b95d4e94b4b324859eea287d1dd
Dylan Attwater <attwater@4231@saskpolytech.ca>
2021-03-18 13:52:57 -0600

    Wrote front end code stubs and started on back end

--- murr-api/src/Entity/Account.php
+++ murr-api/src/Entity/Account.php
@@ -3,8 +3,39 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ArticleRepository;
+use Symfony\Component\Validator\Constraints as Assert;
 
+/**
+ * @ApiResource(
+ *     collectionOperations={"get", "post", "put"},
+ *     itemOperations={"get", "post", "put"}
+ * )
+ * @ORM\Entity(repositoryClass="App\Repository\AccountRepository")
+ * @ORM\Entity(repositoryClass=AccountRepository::class)
+ */
 class Account
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * @Assert\PositiveOrZero(message="The ID has to be zero or a positive number.")
+     */
+    private $id;
 
+    /**
+     * @ORM\Column (type="string", length=20)
+     * @Assert\Length(max="20", maxMessage="First Name cannot be longer than {{ limit }} characters.")
+     * @Assert\NotBlank (message="First name cannot be blank")
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column (type="string", length=20)
+     * @Assert\Length(max="20", maxMessage="Last Name cannot be longer than {{ limit }} characters.")
+     */
+    private $lastName;
 }
--- murr-ui/src/components/AccountInfo.vue
+++ murr-ui/src/components/AccountInfo.vue
@@ -1,13 +1,35 @@
 <template>
-<div>
+  <div>
 
-</div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "AccountInfo"
+export default {
+  name: "AccountInfo",
+  props: {
+    account: {
+      type: Object
     }
+  },
+  data: function () {
+    return {
+      tempAccount: {}
+    }
+  },
+  methods: {
+    getAccountInfo() {
+
+    },
+    saveAccount() {
+
+    },
+    editAccount() {
+
+    }
+  }
+}
+
 </script>
 
 <style scoped>
