44b546453b6bf3db9dd61dd3a35df64503100499
Dylan Attwater <attwater@4231@saskpolytech.ca>
2021-03-22 13:52:47 -0600

    Installed SuperTest, trying different ways to make backend tests

--- murr-api/tests/AccountTest.php
+++ murr-api/tests/AccountTest.php
@@ -17,6 +17,22 @@ class AccountTest
 
     }
 
+    //describe("POST /airports/distance", function () {
+    //    it("calculates the distance between two airports", async function () {
+    //        const response = await request
+    //        .post("/airports/distance")
+    //        .send({ from: "KIX", to: "SFO" });
+    //
+    //    expect(response.status).to.eql(200);
+    //
+    //    const attributes = response.body.data.attributes;
+    //    expect(attributes).to.include.keys("kilometers", "miles", "nautical_miles");
+    //    expect(attributes.kilometers).to.eql(8692.066508240026);
+    //    expect(attributes.miles).to.eql(5397.239853492001);
+    //    expect(attributes.nautical_miles).to.eql(4690.070954910584);
+    //  });
+    //});
+
     /**
      * @test
      */
