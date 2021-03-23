a6b302671fa63373a4c5263f1c0554a0697c8256
Dylan Attwater <attwater@4231@saskpolytech.ca>
2021-03-23 13:50:13 -0600

    Started work on front end tests, updated acceptance tests

--- murr-ui/tests/unit/TestAccountInfo.js
+++ murr-ui/tests/unit/TestAccountInfo.js
@@ -32,3 +32,24 @@ describe('Account Info Update', function () {
 
   })
 })
+
+// describe('POST to the database', () => {
+//   it('creates points for the site and returns status code 200', async function () {
+//     // Creates a mock pickupID to send to the server
+//     const pickup = {
+//       pickupID: 1
+//     }
+//     const response = await request
+//       .post('1')
+//       .send(pickup)
+//     expect(response.status).to.eql(200)
+//     expect(response.text).to.eql('100 Points successfully added to Wascana')
+//   })
+//   it('receive status code 500 while sending no pickupID', async function () {
+//     const newPickup = {}
+//     const response = await request
+//       .post('1')
+//       .send(newPickup)
+//     expect(response.status).to.eql(500)
+//   })
+// })
