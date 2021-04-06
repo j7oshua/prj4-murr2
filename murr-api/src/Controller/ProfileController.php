cdd2680fab7e5276bd1a4c0b574e754be418d59b
Julius Tuba <julius3591@saskpolytech.ca>
2021-04-06 13:57:52 -0600

    Got rid of the id property in Profile.php and made the residentID the primary key. Commented out the function in the ProfileController.php and ProfileRepository.php -- Julius and Dylan

--- murr-api/src/Controller/ProfileController.php
+++ murr-api/src/Controller/ProfileController.php
@@ -5,23 +5,24 @@ namespace App\Controller;
 
 
 use App\Repository\ProfileRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ProfileController
+class ProfileController extends AbstractController
 {
-    /**
-     * @Route ("/profile/{$id}", name=profile)
-     * @param int $id
-     * @param ProfileRepository $ac
-     * @return Response
-     */
-    public function index(int $id, ProfileRepository $ac) : Response
-    {
-        $account = $ac->GetProfileByResidentID($id);
-
-        return $this->json($account);
-    }
+//    /**
+//     * @Route ("/profile/{$id}", name=profile)
+//     * @param int $id
+//     * @param ProfileRepository $ac
+//     * @return Response
+//     */
+//    public function index(int $id, ProfileRepository $ac) : Response
+//    {
+//        $account = $ac->GetProfileByResidentID($id);
+//
+//        return $this->json($account);
+//    }
 
 
 }
--- murr-api/src/Entity/Profile.php
+++ murr-api/src/Entity/Profile.php
@@ -5,47 +5,41 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
-use App\Repository\ArticleRepository;
+use App\Repository\ProfileRepository;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ApiResource(
- *     collectionOperations={"get", "put"},
+ *     collectionOperations={"get", "put", "post"},
  *     itemOperations={"get", "put"}
  * )
- * @ORM\Entity(repositoryClass="ProfileRepository")
  * @ORM\Entity(repositoryClass=ProfileRepository::class)
  */
 class Profile
 {
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     * @Assert\PositiveOrZero(message="The ID has to be zero or a positive number.")
-     */
-    private $id;
 
     /**
+     * @ORM\Id
+     * @ORM\OneToOne(targetEntity="Resident", mappedBy="id")
      * @ORM\Column(type="integer")
      * @Assert\PositiveOrZero
      */
     private $residentID;
 
     /**
-     * @ORM\Column (type="string", length=20)
+     * @ORM\Column (type="string", length=20, nullable=true)
      * @Assert\Length(max="20", min="2", maxMessage="First Name cannot be longer than {{ limit }} characters.", minMessage="First Name must be more than 1 character")
      */
     private $firstName;
 
     /**
-     * @ORM\Column (type="string", length=20)
+     * @ORM\Column (type="string", length=20, nullable=true)
      * @Assert\Length(max="20", maxMessage="Last Name cannot be longer than {{ limit }} characters.")
      */
     private $lastName;
 
     /**
-     * @ORM\Column (type="string")
+     * @ORM\Column (type="string", nullable=true)
      * @Assert\Image(mimeTypes="image/*", mimeTypesMessage="This file is not a valid image.")
      */
     private $profilePic;
@@ -114,11 +108,4 @@ class Profile
         $this->profilePic = $profilePic;
     }
 
-    /**
-     * @return mixed
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
 }
