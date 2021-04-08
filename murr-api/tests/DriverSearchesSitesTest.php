545e775686952b79f65ec852f60122406273b342
RaeAnne Hamel <hamel8982@saskpolytech.ca>
2021-04-08 14:58:43 -0600

    Adjusted the fixtures, so that it will run, and the site database loads with the fixture for the site.yaml, trying to figure out how to put an array of hydra members in to assert json contains. For far fixed the strongly coded errors but ny stuck on implementing the array. --rae

--- murr-api/tests/DriverSearchesSitesTest.php
+++ murr-api/tests/DriverSearchesSitesTest.php
@@ -30,244 +30,160 @@ class DriverSearchesSitesTest extends ApiTestCase
 
         $this->assertResponseStatusCodeSame(200);
         $this->assertJsonContains([
-            '@context' => '/contexts/Site',
-            '@id' => '/Site',
+        '@context' => '/api/context/Site',
+            '@id'=> '/api/sites',
             '@type' => 'hydra:Collection',
-            'hydra:member' => [
-     {
-         '@context' => '/api/contexts/Site',
-   '@id' => '/api/sites/8',
-   '@type' => 'Site',
-   'id' => 8,
-   'siteName' => 'Applewood Bridge',
-   'numBins' => 7
- },
-{
-    '@context' => '/api/contexts/Site',
-   '@id' => '/api/sites/10',
-   '@type' => 'Site',
-   'id' => 10,
-   'siteName' => 'Applewood Gate',
-   'numBins' => 4
- },
-{
-    '@context' => '/api/contexts/Site',
-   '@id' => '/api/sites/2',
-   '@type' => 'Site',
-   'id' => 2,
-   'siteName' => 'Brighton',
-   'numBins' => 5
-},
-{
-    '@context' => '/api/contexts/Site',
-   '@id' => '/api/sites/3',
-   '@type' => 'Site',
-   'id' => 3,
-   'siteName' => 'Britney Manor',
-   'numBins' => 5
-},
-{
-    '@context' => '/api/contexts/Site',
-   '@id' => '/api/sites/9',
-   '@type' => 'Site',
-   'id' => 9,
-   'siteName' => 'Cellsullo Gate',
-   'numBins' => 3
- },
-{
-    '@context' => '/api/contexts/Site',
-   '@id' => '/api/sites/12',
-   '@type' => 'Site',
-   'id' => 12,
-   'siteName' => 'Lucas Caswell Manor',
-   'numBins' => 2
- },
-{
-    '@context' => '/api/contexts/Site',
-   '@id' => '/api/sites/4',
-   '@type' => 'Site',
-   'id' => 4,
-   'siteName' => 'Rosa Towers',
-   'numBins' => 1
- }, {
-            '@context' => '/api/contexts/Site',
-   '@id' => '/api/sites/6',
-   '@type' => 'Site',
-   'id' => 6,
-   'siteName' => 'Roswell Evergreen',
-   'numBins' => 1
- },
-{
-    '@context' => '/api/contexts/Site',
-   '@id' => '/api/sites/5',
-   '@type' => 'Site',
-   'id' => 5,
-   'siteName' => 'Vendetta Suites',
-   'numBins' => 5
- },
-{
-    '@context' => '/api/contexts/Site',
-   '@id' => '/api/sites/11',
-   '@type' => 'Site',
-   'id' => 11,
-   'siteName' => 'Vermont Crossing',
-   'numBins' => 4
- }],
-'hydra:totalItems' => 12,
-        'hydra:view' => {
-        '@id' => '/sites?page=1',
-    '@type' => 'hydra:PartialCollectionView',
-    'hydra:first' => '/sites?page=1',
-        'hydra:last' => '/sites?page=2',
-        'hydra:next' => '/sites?page=2'
-  }
-        ]);
-
-    }
-
-    /**
-     * Title: TestFullSiteList2
-     * Purpose: Check full list page 2
-     * Return: Response code 200 (OK)
-     *          Site objects returned: Wascana and Willowgrove Towers
-     * @test
-     */
-    public function TestFullSiteListAPI2() : void
-    {
-        $response = static::createClient()->request('GET', self::API_URL_SITE_FULL2);
-
-        $this->assertResponseStatusCodeSame(200);
-        $this->assertJsonContains([
-            '@context' => '/contexts/Site',
-            '@id' => '/Site',
-            '@type' => 'hydra:Collection',
-            'hydra:member' => [
-     {
-         '@context' => '/api/contexts/Site',
-         '@id' => '/api/sites/1',
-   '@type' => 'Site',
-   'id' => 1,
-   'siteName' => 'Wascana',
-   'numBins' => 5
- },
-{
-    '@context' => '/api/contexts/Site',
-   '@id' => '/api/sites/7',
-   '@type' => 'Site',
-   'id' => 7,
-   'siteName' => 'Willowgrove Tower',
-   'numBins' => 2
-            }],
-    'hydra:totalItems' => 12,
-    'hydra:view' => {
-    '@id' => '/sites?page=2',
-    '@type' => 'hydra:PartialCollectionView',
-    'hydra:first' => '/sites?page=1',
-    'hydra:last' => '/sites?page=2',
-    'hydra:next' => '/sites?page=2'
-        }]);
-    }
-
-    /**
-     * Title: TestFullSiteName
-     * Purpose: To search the full name Brighton and have it return the Site object for Bright
-     * Return: Response code 200 (OK) and Brighton site object
-     * @test
-     */
-    public function TestFullSiteName() : void
-    {
-        $response = static::createClient()->request('GET', self::API_URL_SITE_TWO);
-
-        $this->assertResponseStatusCodeSame(200);
-        $this->assertJsonContains([
-            '@context' => '/contexts/Site',
-            '@id' => '/Site',
-            '@type' => 'hydra:Collection',
-            'hydra:member' => [
-     {
-         '@context' => '/api/contexts/Site',
-         '@id' => '/api/sites/2',
-         '@type' => 'Site',
-         'id' => 2,
-         'siteName' => 'Brighton',
-         'numBins' => 5
-     }],
-    'hydra:totalItems' => 1,
-    'hydra:view' => {
-        '@id' => '/sites?page=1',
-    '@type' => 'hydra:PartialCollectionView',
-    'hydra:first' => '/sites?page=1',
-    'hydra:last' => '/sites?page=1',
-    'hydra:next' => '/sites?page=1'
-        }]);
-
-    }
-
-    /**
-     * Title: TestPartialName
-     * Purpose: Check a partial Site Name
-     * Return : Response code 200 (OK)
-     *          Site objects returned: Applewood Bridge, Brighton and Britney Manor
-     * @test
-    */
-    public function TestPartialName() : void
-    {
-        $response = static::createClient()->request('GET', self::API_URL_SITE_PART);
-        $this->assertResponseStatusCodeSame(200);
-        $this->assertJsonContains([
-            '@context' => '/contexts/Site',
-            '@id' => '/Site',
-            '@type' => 'hydra:Collection',
-            'hydra:member' => [
-     {
-         '@context' => '/api/contexts/Site',
-         '@id' => '/api/sites/8',
-   '@type' => 'Site',
-   'id' => 8,
-   'siteName' => 'Applewood Bridge',
-   'numBins' => 7
- },
-{
-    '@context' => '/api/contexts/Site',
-   '@id' => '/api/sites/2',
-   '@type' => 'Site',
-   'id' => 2,
-   'siteName' => 'Brighton',
-   'numBins' => 5
-            },
-        {
-            '@context' => '/api/contexts/Site',
-   '@id' => '/api/sites/3',
-   '@type' => 'Site',
-   'id' => 3,
-   'siteName' => 'Britney Manor',
-   'numBins' => 5
-        }],
-    'hydra:totalItems' => 3,
-    'hydra:view' => {
-        '@id' => '/sites?page=1',
-    '@type' => 'hydra:PartialCollectionView',
-    'hydra:first' => '/sites?page=1',
-    'hydra:last' => '/sites?page=1',
-    'hydra:next' => '/sites?page=1'
-        }]);
-    }
-
-    /**
-     * Title: TestSiteNameDoesNotExist
-     *  Purpose: Check if the Site Name enter exists
-     *  Return: Response code 404 (Item not Found)
-     *          Hydra description: 'Item not found for ‘/api/sites?siteName=Wellington’
-     * @test
-     */
-    public function TestSiteNameDoesNotExist() : void
-    {
-        $response = static::createClient()->request('GET', self::API_URL_SITE_NOT_EXIST);
-
-        $this->assertResponseStatusCodeSame(404);
-        $this->assertJsonContains([
-            'hydra:description'=> 'Item not found for ‘/api/sites?siteName=Wellington’'
+            'hydra:totalItems' => 10,
+            'hydra:view' => [
+                // here is where we need to figure out how to put an array of sites in
+                '@id' => 'api/sites/8'
+            ],
         ]);
     }
+//
+//    /**
+//     * Title: TestFullSiteList2
+//     * Purpose: Check full list page 2
+//     * Return: Response code 200 (OK)
+//     *          Site objects returned: Wascana and Willowgrove Towers
+//     * @test
+//     */
+//    public function TestFullSiteListAPI2() : void
+//    {
+//        $response = static::createClient()->request('GET', self::API_URL_SITE_FULL2);
+//
+//        $this->assertResponseStatusCodeSame(200);
+//        $this->assertJsonContains([
+//            '@context' => '/contexts/Site',
+//            '@id' => '/Site',
+//            '@type' => 'hydra:Collection',
+//            'hydra:member' => [
+//     {
+//         '@context' => '/api/contexts/Site',
+//         '@id' => '/api/sites/1',
+//   '@type' => 'Site',
+//   'id' => 1,
+//   'siteName' => 'Wascana',
+//   'numBins' => 5
+// },
+//{
+//    '@context' => '/api/contexts/Site',
+//   '@id' => '/api/sites/7',
+//   '@type' => 'Site',
+//   'id' => 7,
+//   'siteName' => 'Willowgrove Tower',
+//   'numBins' => 2
+//            }],
+//    'hydra:totalItems' => 12,
+//    'hydra:view' => {
+//    '@id' => '/sites?page=2',
+//    '@type' => 'hydra:PartialCollectionView',
+//    'hydra:first' => '/sites?page=1',
+//    'hydra:last' => '/sites?page=2',
+//    'hydra:next' => '/sites?page=2'
+//        }]);
+//    }
+//
+//    /**
+//     * Title: TestFullSiteName
+//     * Purpose: To search the full name Brighton and have it return the Site object for Bright
+//     * Return: Response code 200 (OK) and Brighton site object
+//     * @test
+//     */
+//    public function TestFullSiteName() : void
+//    {
+//        $response = static::createClient()->request('GET', self::API_URL_SITE_TWO);
+//
+//        $this->assertResponseStatusCodeSame(200);
+//        $this->assertJsonContains([
+//            '@context' => '/contexts/Site',
+//            '@id' => '/Site',
+//            '@type' => 'hydra:Collection',
+//            'hydra:member' => [
+//     {
+//         '@context' => '/api/contexts/Site',
+//         '@id' => '/api/sites/2',
+//         '@type' => 'Site',
+//         'id' => 2,
+//         'siteName' => 'Brighton',
+//         'numBins' => 5
+//     }],
+//    'hydra:totalItems' => 1,
+//    'hydra:view' => {
+//        '@id' => '/sites?page=1',
+//    '@type' => 'hydra:PartialCollectionView',
+//    'hydra:first' => '/sites?page=1',
+//    'hydra:last' => '/sites?page=1',
+//    'hydra:next' => '/sites?page=1'
+//        }]);
+//
+//    }
+//
+//    /**
+//     * Title: TestPartialName
+//     * Purpose: Check a partial Site Name
+//     * Return : Response code 200 (OK)
+//     *          Site objects returned: Applewood Bridge, Brighton and Britney Manor
+//     * @test
+//    */
+//    public function TestPartialName() : void
+//    {
+//        $response = static::createClient()->request('GET', self::API_URL_SITE_PART);
+//        $this->assertResponseStatusCodeSame(200);
+//        $this->assertJsonContains([
+//            '@context' => '/contexts/Site',
+//            '@id' => '/Site',
+//            '@type' => 'hydra:Collection',
+//            'hydra:member' => [
+//     {
+//         '@context' => '/api/contexts/Site',
+//         '@id' => '/api/sites/8',
+//   '@type' => 'Site',
+//   'id' => 8,
+//   'siteName' => 'Applewood Bridge',
+//   'numBins' => 7
+// },
+//{
+//    '@context' => '/api/contexts/Site',
+//   '@id' => '/api/sites/2',
+//   '@type' => 'Site',
+//   'id' => 2,
+//   'siteName' => 'Brighton',
+//   'numBins' => 5
+//            },
+//        {
+//            '@context' => '/api/contexts/Site',
+//   '@id' => '/api/sites/3',
+//   '@type' => 'Site',
+//   'id' => 3,
+//   'siteName' => 'Britney Manor',
+//   'numBins' => 5
+//        }],
+//    'hydra:totalItems' => 3,
+//    'hydra:view' => {
+//        '@id' => '/sites?page=1',
+//    '@type' => 'hydra:PartialCollectionView',
+//    'hydra:first' => '/sites?page=1',
+//    'hydra:last' => '/sites?page=1',
+//    'hydra:next' => '/sites?page=1'
+//        }]);
+//    }
+//
+//    /**
+//     * Title: TestSiteNameDoesNotExist
+//     *  Purpose: Check if the Site Name enter exists
+//     *  Return: Response code 404 (Item not Found)
+//     *          Hydra description: 'Item not found for ‘/api/sites?siteName=Wellington’
+//     * @test
+//     */
+//    public function TestSiteNameDoesNotExist() : void
+//    {
+//        $response = static::createClient()->request('GET', self::API_URL_SITE_NOT_EXIST);
+//
+//        $this->assertResponseStatusCodeSame(404);
+//        $this->assertJsonContains([
+//            'hydra:description'=> 'Item not found for ‘/api/sites?siteName=Wellington’'
+//        ]);
+//    }
 
 }
