d36bc8d6fa40338c06a55203a36d564db5129389
Lucas Tom <tom1608@saskpolytech.ca>
2021-04-08 13:54:47 -0600

    worked on the automated test. added more tests to the api front end and reworked the back end test for proper format. it is giving me strongly typed errors and I do not know why. also updating docs as we go. --Lucas

--- murr-api/tests/DriverSearchesSitesTest.php
+++ murr-api/tests/DriverSearchesSitesTest.php
@@ -11,15 +11,166 @@ class DriverSearchesSitesTest extends ApiTestCase
     use RefreshDatabaseTrait;
 
     // The URLs need to test
-    const API_URL_SITE_TWO = 'http://localhost:8000/api/sites?siteName=Brighton';
-    const API_URL_SITE_NULL = 'http://localhost:8000/api/sites?siteName=';
-    const API_URL_SITE_PART = 'http://localhost:8000/api/sites?siteName=Bri';
-    const API_URL_SITE_NOT_EXIST = 'http://localhost:8000/api/sites?siteName=Wellington';
+    const API_URL_SITE_TWO = 'http://localhost:8000/api/sites?order[siteName=Brighton]=desc&page=1';
+    const API_URL_SITE_FULL = 'http://localhost:8000/api/sites?order[siteName=]=desc&page=1';
+    const API_URL_SITE_FULL2 = 'http://localhost:8000/api/sites?order[siteName=]=desc&page=2';
+    const API_URL_SITE_PART = 'http://localhost:8000/api/sites?order[siteName=Bri]=desc&page=1';
+    const API_URL_SITE_NOT_EXIST = 'http://localhost:8000/api/sites?order[siteName=Wellington]=desc&page=1';
 
+    /**
+     * Title: TestFullSiteList
+     * Purpose: Check full list page 1
+     * Return: Response code 200 (OK)
+     *          Site objects returned: Applewood bridge, Applewood Gate, Brighton, Britney Manor, Cellsullo Gate, Lucas Caswell Manor, Rosa Towers, Vendetta Suites, Vermont Crossing
+     * @test
+     */
+    public function TestFullSiteListAPI() : void
+    {
+        $response = static::createClient()->request('GET', self::API_URL_SITE_FULL);
+
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertJsonContains([
+            '@context' => '/contexts/Site',
+            '@id' => '/Site',
+            '@type' => 'hydra:Collection',
+            'hydra:member' => [
+     {
+         '@context' => '/api/contexts/Site',
+   '@id' => '/api/sites/8',
+   '@type' => 'Site',
+   'id' => 8,
+   'siteName' => 'Applewood Bridge',
+   'numBins' => 7
+ },
+{
+    '@context' => '/api/contexts/Site',
+   '@id' => '/api/sites/10',
+   '@type' => 'Site',
+   'id' => 10,
+   'siteName' => 'Applewood Gate',
+   'numBins' => 4
+ },
+{
+    '@context' => '/api/contexts/Site',
+   '@id' => '/api/sites/2',
+   '@type' => 'Site',
+   'id' => 2,
+   'siteName' => 'Brighton',
+   'numBins' => 5
+},
+{
+    '@context' => '/api/contexts/Site',
+   '@id' => '/api/sites/3',
+   '@type' => 'Site',
+   'id' => 3,
+   'siteName' => 'Britney Manor',
+   'numBins' => 5
+},
+{
+    '@context' => '/api/contexts/Site',
+   '@id' => '/api/sites/9',
+   '@type' => 'Site',
+   'id' => 9,
+   'siteName' => 'Cellsullo Gate',
+   'numBins' => 3
+ },
+{
+    '@context' => '/api/contexts/Site',
+   '@id' => '/api/sites/12',
+   '@type' => 'Site',
+   'id' => 12,
+   'siteName' => 'Lucas Caswell Manor',
+   'numBins' => 2
+ },
+{
+    '@context' => '/api/contexts/Site',
+   '@id' => '/api/sites/4',
+   '@type' => 'Site',
+   'id' => 4,
+   'siteName' => 'Rosa Towers',
+   'numBins' => 1
+ }, {
+            '@context' => '/api/contexts/Site',
+   '@id' => '/api/sites/6',
+   '@type' => 'Site',
+   'id' => 6,
+   'siteName' => 'Roswell Evergreen',
+   'numBins' => 1
+ },
+{
+    '@context' => '/api/contexts/Site',
+   '@id' => '/api/sites/5',
+   '@type' => 'Site',
+   'id' => 5,
+   'siteName' => 'Vendetta Suites',
+   'numBins' => 5
+ },
+{
+    '@context' => '/api/contexts/Site',
+   '@id' => '/api/sites/11',
+   '@type' => 'Site',
+   'id' => 11,
+   'siteName' => 'Vermont Crossing',
+   'numBins' => 4
+ }],
+'hydra:totalItems' => 12,
+        'hydra:view' => {
+        '@id' => '/sites?page=1',
+    '@type' => 'hydra:PartialCollectionView',
+    'hydra:first' => '/sites?page=1',
+        'hydra:last' => '/sites?page=2',
+        'hydra:next' => '/sites?page=2'
+  }
+        ]);
+
+    }
+
+    /**
+     * Title: TestFullSiteList2
+     * Purpose: Check full list page 2
+     * Return: Response code 200 (OK)
+     *          Site objects returned: Wascana and Willowgrove Towers
+     * @test
+     */
+    public function TestFullSiteListAPI2() : void
+    {
+        $response = static::createClient()->request('GET', self::API_URL_SITE_FULL2);
+
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertJsonContains([
+            '@context' => '/contexts/Site',
+            '@id' => '/Site',
+            '@type' => 'hydra:Collection',
+            'hydra:member' => [
+     {
+         '@context' => '/api/contexts/Site',
+         '@id' => '/api/sites/1',
+   '@type' => 'Site',
+   'id' => 1,
+   'siteName' => 'Wascana',
+   'numBins' => 5
+ },
+{
+    '@context' => '/api/contexts/Site',
+   '@id' => '/api/sites/7',
+   '@type' => 'Site',
+   'id' => 7,
+   'siteName' => 'Willowgrove Tower',
+   'numBins' => 2
+            }],
+    'hydra:totalItems' => 12,
+    'hydra:view' => {
+    '@id' => '/sites?page=2',
+    '@type' => 'hydra:PartialCollectionView',
+    'hydra:first' => '/sites?page=1',
+    'hydra:last' => '/sites?page=2',
+    'hydra:next' => '/sites?page=2'
+        }]);
+    }
 
     /**
      * Title: TestFullSiteName
-     * Purpose: To search teh full name Brighton and have it return the Site object for Bright
+     * Purpose: To search the full name Brighton and have it return the Site object for Bright
      * Return: Response code 200 (OK) and Brighton site object
      * @test
      */
@@ -29,25 +180,26 @@ class DriverSearchesSitesTest extends ApiTestCase
 
         $this->assertResponseStatusCodeSame(200);
         $this->assertJsonContains([
-        '@context'=> '/api/contexts/Site',
-        '@id'=> '/api/sites/2',
-        '@type'=> 'Site',
-        'id'=> 2,
-        'siteName'=> 'Brighton',
-        'numBins'=> 5,
-        ]);
-
-    }
-
-    /**
-     * Title: TestNullSite
-     * Purpose: Check  A null (oe empty site) request
-     * Return: Response code 404 (Item not Found)
-     *          Hydra description: 'Item not found for ‘/api/sites?siteName=’
-     * @test
-     */
-    public function TestNullSite() : void
-    {
+            '@context' => '/contexts/Site',
+            '@id' => '/Site',
+            '@type' => 'hydra:Collection',
+            'hydra:member' => [
+     {
+         '@context' => '/api/contexts/Site',
+         '@id' => '/api/sites/2',
+         '@type' => 'Site',
+         'id' => 2,
+         'siteName' => 'Brighton',
+         'numBins' => 5
+     }],
+    'hydra:totalItems' => 1,
+    'hydra:view' => {
+        '@id' => '/sites?page=1',
+    '@type' => 'hydra:PartialCollectionView',
+    'hydra:first' => '/sites?page=1',
+    'hydra:last' => '/sites?page=1',
+    'hydra:next' => '/sites?page=1'
+        }]);
 
     }
 
@@ -55,7 +207,7 @@ class DriverSearchesSitesTest extends ApiTestCase
      * Title: TestPartialName
      * Purpose: Check a partial Site Name
      * Return : Response code 200 (OK)
-     *          Site objects returned: Brighton site object and Britney Manor
+     *          Site objects returned: Applewood Bridge, Brighton and Britney Manor
      * @test
     */
     public function TestPartialName() : void
@@ -63,29 +215,42 @@ class DriverSearchesSitesTest extends ApiTestCase
         $response = static::createClient()->request('GET', self::API_URL_SITE_PART);
         $this->assertResponseStatusCodeSame(200);
         $this->assertJsonContains([
-            [
-                '@context'=> '/api/contexts/Site',
-                '@id'=> '/api/sites/8',
-                '@type'=> 'Site',
-                'id'=> 8,
-                'siteName'=> 'Applewood Bridge',
-                'numBins'=> 7,
-            ],[
-            '@context'=> '/api/contexts/Site',
-            '@id'=> '/api/sites/2',
-            '@type'=> 'Site',
-            'id'=> 2,
-            'siteName'=> 'Brighton',
-            'numBins'=> 5,
-            ],[
-            '@context'=> '/api/contexts/Site',
-            '@id'=> '/api/sites/3',
-            '@type'=> 'Site',
-            'id'=> 3,
-            'siteName'=> 'Britney Manor',
-            'numBins'=> 5
-            ]
-        ]);
+            '@context' => '/contexts/Site',
+            '@id' => '/Site',
+            '@type' => 'hydra:Collection',
+            'hydra:member' => [
+     {
+         '@context' => '/api/contexts/Site',
+         '@id' => '/api/sites/8',
+   '@type' => 'Site',
+   'id' => 8,
+   'siteName' => 'Applewood Bridge',
+   'numBins' => 7
+ },
+{
+    '@context' => '/api/contexts/Site',
+   '@id' => '/api/sites/2',
+   '@type' => 'Site',
+   'id' => 2,
+   'siteName' => 'Brighton',
+   'numBins' => 5
+            },
+        {
+            '@context' => '/api/contexts/Site',
+   '@id' => '/api/sites/3',
+   '@type' => 'Site',
+   'id' => 3,
+   'siteName' => 'Britney Manor',
+   'numBins' => 5
+        }],
+    'hydra:totalItems' => 3,
+    'hydra:view' => {
+        '@id' => '/sites?page=1',
+    '@type' => 'hydra:PartialCollectionView',
+    'hydra:first' => '/sites?page=1',
+    'hydra:last' => '/sites?page=1',
+    'hydra:next' => '/sites?page=1'
+        }]);
     }
 
     /**
--- murr-ui/tests/unit/TestDriverSearchSiteAPI.spec.js
+++ murr-ui/tests/unit/TestDriverSearchSiteAPI.spec.js
@@ -2,12 +2,11 @@ const request = require('supertest')('http://127.0.0.1:8000/site/')
 const expect = require('chai').expect
 
 describe('GET /sites', function () {
-
   /**
    *
    */
-  it('Display all site when first arriving to the page', async function() {
-    const response = await request.get('/sites')
+  it('Display all site when first arriving to the page', async function () {
+    const response = await request.get('/sites/?order[site.siteName=]=desc&page=1')
     expect(response.status).to.eql(200)
     expect(response.body['hydra:member'][0]).to.contain({ id: 8 })
     expect(response.body['hydra:member'][1]).to.contain({ id: 10 })
@@ -16,12 +15,9 @@ describe('GET /sites', function () {
     expect(response.body['hydra:member'][4]).to.contain({ id: 9 })
     expect(response.body['hydra:member'][5]).to.contain({ id: 12 })
     expect(response.body['hydra:member'][6]).to.contain({ id: 4 })
-    expect(response.body['hydra:member'][7]).to.contain({ id:6 })
+    expect(response.body['hydra:member'][7]).to.contain({ id: 6 })
     expect(response.body['hydra:member'][8]).to.contain({ id: 5 })
     expect(response.body['hydra:member'][9]).to.contain({ id: 11 })
-    expect(response.body['hydra:member'][10]).to.contain({ id: 1 })
-    expect(response.body['hydra:member'][11]).to.contain({ id: 7 })
-
     expect(response.body['hydra:member'][0]).to.contain({ siteName: 'Applewood Bridge' })
     expect(response.body['hydra:member'][1]).to.contain({ siteName: 'Applewood Gate' })
     expect(response.body['hydra:member'][2]).to.contain({ siteName: 'Brighton' })
@@ -32,7 +28,50 @@ describe('GET /sites', function () {
     expect(response.body['hydra:member'][7]).to.contain({ siteName: 'Rosewell Evergreen' })
     expect(response.body['hydra:member'][8]).to.contain({ siteName: 'Vendetta Suites' })
     expect(response.body['hydra:member'][9]).to.contain({ siteName: 'Vermont Crossing' })
+  })
+
+  /**
+   *
+   */
+  it('Display all sites on page 2', async function () {
+    const response = await request.get('/sites/?order[site.siteName=]=desc&page=2')
+    expect(response.status).to.eql(200)
+    expect(response.body['hydra:member'][10]).to.contain({ id: 1 })
+    expect(response.body['hydra:member'][11]).to.contain({ id: 7 })
     expect(response.body['hydra:member'][10]).to.contain({ siteName: 'Wascana' })
     expect(response.body['hydra:member'][11]).to.contain({ siteName: 'Willowgrove Towers' })
+  })
+
+  /**
+   *
+   */
+  it('Display one site when searching with "Brighton"', async function () {
+    const response = await request.get('/sites/?order[site.siteName=Brighton]=desc&page=1')
+    expect(response.status).to.eql(200)
+    expect(response.body['hydra:member'][0]).to.contain({ id: 2 })
+    expect(response.body['hydra:member'][0]).to.contain({ siteName: 'Brighton' })
+  })
+
+  /**
+   *
+   */
+  it('Display three sites when searching with "Bri"', async function () {
+    const response = await request.get('/sites/?order[site.siteName=Bri]=desc&page=1')
+    expect(response.status).to.eql(200)
+    expect(response.body['hydra:member'][1]).to.contain({ id: 8 })
+    expect(response.body['hydra:member'][1]).to.contain({ siteName: 'Applewood Bridge' })
+    expect(response.body['hydra:member'][1]).to.contain({ id: 2 })
+    expect(response.body['hydra:member'][1]).to.contain({ siteName: 'Brighton' })
+    expect(response.body['hydra:member'][1]).to.contain({ id: 3 })
+    expect(response.body['hydra:member'][1]).to.contain({ siteName: 'Britney Manor' })
+  })
+
+  /**
+   *
+   */
+  it('Display an error for searching a site that does not exist', async function () {
+    const response = await request.get('/sites/?order[site.siteName=Wellington]=desc&page=1')
+    expect(response.status).to.eql(404)
+    expect(response.body['hydra:description']).to.contain('Item not found for ‘/api/sites?siteName=Wellington')
   })
 })
