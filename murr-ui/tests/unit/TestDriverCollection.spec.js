75346fb4e5c1ac3a9cc3ea2507350741f632705c
RaeAnne Hamel <hamel8982@saskpolytech.ca>
2021-04-18 15:35:44 -0600

    i thought my oull worked perviously but it did not so it merge when I was trying to push my stuff. fixed any merging errors --Rae

--- murr-ui/tests/unit/TestDriverCollection.spec.js
+++ murr-ui/tests/unit/TestDriverCollection.spec.js
@@ -28,15 +28,10 @@ describe('DriverCollection', () => {
    */
   it('Should display error message and site list with the input Wellington', async () => {
     const Search = wrapper.find('#filter-input')
-<<<<<<< HEAD
+
     wrapper.find('#filter-input').trigger('Keypress')
     Search.element.value = 'Wellington'
     expect(wrapper.find('#filter-input').element.value).to.equal('Wellington')
-=======
-    await Search.setValue('Wellington')
-    expect(wrapper.find('#filter-input').element.value).to.equal('Wellington')
-    wrapper.find('#filter-input').trigger('keypress')
->>>>>>> 009efe0d372b9550f35ab47bba859da59ad6bd99
     expect(wrapper.html().includes('No site found with that criteria'))
   })
 
@@ -45,21 +40,11 @@ describe('DriverCollection', () => {
    */
   it('AShould display 3 sites in aplphabetical order acsending sortedBy name', async () => {
     const Search = wrapper.find('#filter-input')
-<<<<<<< HEAD
     Search.element.value = 'Bri'
     expect(wrapper.find('#filter-input').element.value).to.equal('Bri')
     wrapper.find('#filter-input').trigger('Keypress')
     expect(wrapper.html().includes('Applewood Bridge'))
     expect(wrapper.html().includes('Brighton'))
     expect(wrapper.html().includes('Britney Manor'))
-=======
-    await Search.setValue('Bri')
-    expect(wrapper.find('#filter-input').element.value).to.equal('Bri')
-    wrapper.find('#filter-input').trigger('keypress')
-    expect(wrapper.find('#my-table').text()).to.equal('Applewood Bridge')
-    expect(wrapper.find('#my-table').text()).to.equal('Brighton')
-    expect(wrapper.find('#my-table').text()).to.equal('Britney Manor')
-    expect([{ id: 8, name: 'Applewood Bridge' }, { id: 2, name: 'Brighton' }, { id: 3, name: 'Britney Manor' }]).to.be.sortedBy('name')
->>>>>>> 009efe0d372b9550f35ab47bba859da59ad6bd99
   })
 })
