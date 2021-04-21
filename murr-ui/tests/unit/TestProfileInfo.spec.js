fdb0c4f03d3f52b03237c1ff4ad6dd0893b4b191
Dylan Attwater <attwater@4231@saskpolytech.ca>
2021-04-21 15:55:25 -0600

    Attempted to do a quick fix on front end tests using MutationObserver, but it did not work, changed front end tests to only include information that was actually being checked, still a false positive

--- murr-ui/tests/unit/TestProfileInfo.spec.js
+++ murr-ui/tests/unit/TestProfileInfo.spec.js
@@ -30,126 +30,79 @@ describe('Points', () => {
   })
 
   it('Should successfully render first name on the first name input', async () => {
+    expect(wrapper.find('#editProfileTitle').exists()).to.equal(false)
     await wrapper.setData({
+      tempProfile: {
+        firstName: 'Tom'
+      },
       editMode: true
     })
-    // await wrapper.find('#btnEditOrSave').trigger('click')
-    // console.log(wrapper.find('#btnEditOrSave').html())
-    const inputFirst = wrapper.find('#firstNameInput')
-    inputFirst.element.value = 'Tom'
     expect(wrapper.find('#editProfileTitle').text()).to.equal('Edit Profile Information')
-    expect(wrapper.find('#firstNameInput').element.value).to.equal('Tom')
     expect(wrapper.find('#validFirstName').text()).to.equal('The first name is valid')
   })
 
   it('Should unsuccessfully update with too long first name', async () => {
-    wrapper.find('#btnEditOrSave').trigger('click')
-    await wrapper.setData({
-      editMode: true
-    })
-    const inputFirst = wrapper.find('#firstNameInput')
-    inputFirst.element.value = 'l'.repeat(21)
-    expect(wrapper.find('#editProfileTitle').text()).to.equal('Edit Profile Information')
-    expect(wrapper.find('#firstNameInput').element.value).to.equal('l'.repeat(21))
     await wrapper.setData({
       tempProfile: {
-        firstName: 'l'.repeat(21)
-      }
+        firstName: 'f'.repeat(21)
+      },
+      editMode: true
     })
     expect(wrapper.find('#invalidFirstName').text()).to.equal('First Name cannot be longer than 20 characters')
   })
 
   it('Should successfully render last name on the last name input', async () => {
-    wrapper.find('#btnEditOrSave').trigger('click')
     await wrapper.setData({
+      tempProfile: {
+        lastName: 'Andrews'
+      },
       editMode: true
     })
-    const inputLast = wrapper.find('#lastNameInput')
-    inputLast.element.value = 'Andrews'
-    expect(wrapper.find('#editProfileTitle').text()).to.equal('Edit Profile Information')
-    expect(wrapper.find('#lastNameInput').element.value).to.equal('Andrews')
     expect(wrapper.find('#validLastName').text()).to.equal('The last name is valid')
   })
 
   it('Should unsuccessfully update with too long last name', async () => {
-    wrapper.find('#btnEditOrSave').trigger('click')
-    await wrapper.setData({
-      editMode: true
-    })
-    const inputLast = wrapper.find('#lastNameInput')
-    inputLast.element.value = 'l'.repeat(21)
-    expect(wrapper.find('#editProfileTitle').text()).to.equal('Edit Profile Information')
-    expect(wrapper.find('#lastNameInput').element.value).to.equal('l'.repeat(21))
     await wrapper.setData({
       tempProfile: {
         lastName: 'l'.repeat(21)
-      }
+      },
+      editMode: true
     })
     expect(wrapper.find('#invalidLastName').text()).to.equal('Last Name cannot be longer than 20 characters')
   })
 
   it('Should successfully update with valid image file for profile picture', async () => {
-    await wrapper.setProps({
-      residentID: 1,
-      showModal: true,
-      profile: {
-        firstName: 'Mark',
-        lastName: 'Jacobs',
-        profilePic: null
-      }
-    })
-    await wrapper.setData({
-      editMode: true
-    })
-    wrapper.find('#btnEditOrSave').trigger('click')
-    expect(wrapper.find('#editProfileTitle').text()).to.equal('Edit Profile Information')
     await wrapper.setData({
       file: {
         name: 'test.jpg',
-        size: 50000,
+        size: (1024 * 1024 * 2),
         type: 'image/jpg'
-      }
+      },
+      editMode: true
     })
     expect(wrapper.find('#validProfPicSize').text()).to.equal('Profile pic is valid')
   })
 
   it('Should unsuccessfully update with invalid too large image for profile picture', async () => {
-    // wrapper.find('#btnEditOrSave').trigger('click')
-    await wrapper.setData({
-      editMode: true
-    })
-    expect(wrapper.find('#editProfileTitle').text()).to.equal('Edit Profile Information')
     await wrapper.setData({
       file: {
-        name: 'test2.jpg',
-        size: 5000000000,
+        name: 'test.jpg',
+        size: (1024 * 1024 * 2 + 1),
         type: 'image/jpg'
-      }
+      },
+      editMode: true
     })
     expect(wrapper.find('#invalidProfPicSize').text()).to.equal('Profile pic cannot be larger than 2MB')
   })
 
   it('Should unsuccessfully render with all the inputs empty', async () => {
-    wrapper.find('#btnEditOrSave').trigger('click')
-    await wrapper.setData({
-      editMode: true
-    })
-    const inputFirst = wrapper.find('#firstNameInput')
-    const inputLast = wrapper.find('#lastNameInput')
-    const inputProfPic = wrapper.find('#profPicInput')
-    inputFirst.element.value = ''
-    inputLast.element.value = ''
-    inputProfPic.element.value = ''
-    expect(wrapper.find('#editProfileTitle').text()).to.equal('Edit Profile Information')
-    expect(wrapper.find('#firstNameInput').element.value).to.equal('')
-    expect(wrapper.find('#lastNameInput').element.value).to.equal('')
-    expect(wrapper.find('#profPicInput').element.value).to.equal('')
     await wrapper.setData({
       tempProfile: {
         firstName: '',
         lastName: ''
       },
-      file: []
+      file: [],
+      editMode: true
     })
     expect(wrapper.find('#validFirstName').text()).to.equal('The first name is valid')
     expect(wrapper.find('#validLastName').text()).to.equal('The last name is valid')
